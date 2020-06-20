5ae9032c937d7497c0a1098772b8b8ba486f259e
lee <ce.li@thoughtworks.com>
2020-06-20 23:32:21 +0800

    add css style

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 import "./style/index.scss";
-import { sendRequest } from "./sendRequest";
+import { sendRequestForUser, sendRequestForEducations } from "./sendRequest";
 
 const url = window.location.href;
 const id = url.split("users/")[1];
 
-const onOrderChange = async () => {
-  const user = await sendRequest(id);
+const getUserInfo = async () => {
+  const user = await sendRequestForUser(id);
   document.getElementById("name").innerHTML = user.name;
   document.getElementById("age").innerHTML = user.age;
   document.getElementById("description").innerHTML = user.description;
 };
 
-onOrderChange();
+const getUserEducations = async () => {
+  const educations = await sendRequestForEducations(id);
+  console.log(educations);
+  // const container = document.getElementById("container");
+  // for (var i = 0; i < educations.length; i++) {
+  //   if (i === 0) {
+  //     container.innerHTML += '<ul><li>' + educations[i] + '</li></ul>';
+  //   } else {
+  //     container.childNodes[0].innerHTML += '<li>' + educations[i] + '</li>';
+  //   }
+  // }
+};
+
+getUserInfo();
+getUserEducations();
--- src/sendRequest.js
+++ src/sendRequest.js
@@ -2,7 +2,12 @@ import axios from "axios";
 
 const baseURL = "http://localhost:8080/users";
 
-export const sendRequest = async (id) => {
+export const sendRequestForUser = async (id) => {
   const response = await axios.get(`${baseURL}/${id}`);
+  return response.data;
+};
+
+export const sendRequestForEducations = async (id) => {
+  const response = await axios.get(`${baseURL}/${id}/educations`);
   return response.data;
 };
